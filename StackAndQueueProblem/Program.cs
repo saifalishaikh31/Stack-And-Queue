bf1de435520578e9602f90fdb726064c86dc9c2c
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-16 15:48:20 +0530

    [Saif Ali] Add : Ability to create a stack Added

---
+++ StackAndQueueProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(56);
+            linkedListStack.Push(30);
+            linkedListStack.Push(70);
+            linkedListStack.Display();
+            Console.ReadLine();
+        }
+    }
+}
