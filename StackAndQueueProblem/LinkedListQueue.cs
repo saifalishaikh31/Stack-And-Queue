99e67da1c16cc4b1646c942ba062ed8f6335df4f
saifalishaikh31 <rockyasaifalishaikh@gmail.com>
2022-02-16 20:42:54 +0530

    [Saif Ali] Add : Ability to create a Queue Added

---
+++ StackAndQueueProblem/LinkedListQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueProblem
+{
+    internal class LinkedListQueue
+    {
+        Node head = null;
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into Queue", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty!");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- StackAndQueueProblem/Program.cs
+++ StackAndQueueProblem/Program.cs
@@ -10,13 +10,41 @@ namespace StackAndQueueProblem
     {
         static void Main(string[] args)
         {
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.Push(70);
-            linkedListStack.Push(30);
-            linkedListStack.Push(56);
-            linkedListStack.Display();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\n 1.LinkedList Stack."
+                                + "\n 2.LinkedList Queue."
+                                + "\n 3.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("LinkedList Stack");
+                        LinkedListStack linkedListStack = new LinkedListStack();
+                        linkedListStack.Push(70);
+                        linkedListStack.Push(30);
+                        linkedListStack.Push(56);
+                        linkedListStack.Display();
+                        linkedListStack.IsEmpty();
+                        linkedListStack.Display();
+                        break;
+                    case 2:
+                        Console.WriteLine("LinkedList Queue");
+                        LinkedListQueue linkedListQueue = new LinkedListQueue();
+                        linkedListQueue.Enqueue(56);
+                        linkedListQueue.Enqueue(30);
+                        linkedListQueue.Enqueue(70);
+                        linkedListQueue.Display();
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter Correct Option!!!");
+                        break;
+                }
+            }
             Console.ReadLine();
         }
     }
